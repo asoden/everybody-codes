b08c891147e97f8fd2630ed9c780a9ea2ca57297
Andrew Soden <andrew.soden@aviture.us.com>
2026-02-15 21:42:36 -0600

    Add benchmarks for memory usage and runtime

---
+++ benches/memory.rs
@@ -0,0 +1,76 @@
+use divan::AllocProfiler;
+
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
+
+fn main() {
+    divan::main();
+}
+
+macro_rules! benchmark {
+    ($event:tt $($quest:tt),*) => {
+        #[divan::bench_group(
+            name = stringify!($event),
+            sample_count = 1,
+        )]
+        mod $event {
+            $(
+            #[divan::bench_group()]
+            mod $quest {
+                use std::fs::read_to_string;
+                use everybody_codes::util::ansi::*;
+                use everybody_codes::$event::$quest;
+                use std::sync::LazyLock;
+
+                static DATA1: LazyLock<String> = LazyLock::new(|| {
+                    let event = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($event)).parse::<u32>().unwrap();
+                    let quest = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($quest)).parse::<u32>().unwrap();
+                    let path = format!("input/{event:02}/everybody_codes_e{event}_q{quest:02}_p1.txt");
+                    read_to_string(&path).unwrap_or_else(|_| {
+                        panic!("Missing input file {BOLD}{WHITE}{path}{RESET}");
+                    })
+                });
+
+                static DATA2: LazyLock<String> = LazyLock::new(||{
+                    let event = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($event)).parse::<u32>().unwrap();
+                    let quest = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($quest)).parse::<u32>().unwrap();
+                    let path = format!("input/{event:02}/everybody_codes_e{event}_q{quest:02}_p2.txt");
+
+                    read_to_string(&path).unwrap_or_else(|_| {
+                        panic!("Missing input file {BOLD}{WHITE}{path}{RESET}");
+                    })
+                });
+
+                static DATA3: LazyLock<String> = LazyLock::new(|| {
+
+                    let event = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($event)).parse::<u32>().unwrap();
+                    let quest = everybody_codes::util::tools::remove_non_digits_from_start(stringify!($quest)).parse::<u32>().unwrap();
+                    let path = format!("input/{event:02}/everybody_codes_e{event}_q{quest:02}_p3.txt");
+
+                    read_to_string(&path).unwrap_or_else(|_| {
+                        panic!("Missing input file {BOLD}{WHITE}{path}{RESET}");
+                    })
+                });
+
+                #[divan::bench()]
+                fn part1() {
+                    $quest::part1(divan::black_box(&DATA1));
+                }
+
+                #[divan::bench()]
+                fn part2() {
+                    $quest::part2(divan::black_box(&DATA2));
+                }
+
+                #[divan::bench()]
+                fn part3() {
+                    $quest::part3(divan::black_box(&DATA3));
+                }
+            })*
+        }
+    };
+}
+
+benchmark!(event2024
+    quest01, quest02, quest03, quest04, quest05, quest06, quest07, quest08, quest09, quest10
+);
