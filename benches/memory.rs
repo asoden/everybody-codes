6fdc186c5ce33473c701ba2c84d4ae937241d86e
Andrew Soden <andrew.soden@aviture.us.com>
2026-02-22 22:33:00 -0600

    update memory benchmark

--- benches/memory.rs
+++ benches/memory.rs
@@ -72,5 +72,6 @@ macro_rules! benchmark {
 }
 
 benchmark!(event2024
-    quest01, quest02, quest03, quest04, quest05, quest06, quest07, quest08, quest09, quest10
+    quest01, quest02, quest03, quest04, quest05, quest06, quest07, quest08, quest09, quest10,
+    quest11, quest12, quest13, quest14, quest15
 );
